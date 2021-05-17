484f97d0a83c2957ed6babe5e5cb8b7c72b745bc
gbwsh <70608214+gbwsh@users.noreply.github.com>
2021-05-17 12:23:57 +0200

    Update wordCount.py

--- wordCount.py
+++ wordCount.py
@@ -1,11 +1,16 @@
+#wordCount function uses regex to iterate over each word in a given string
+#and prints out how many times each word has been used, case insensitive
+
 import re, pprint
 wordRe = re.compile(r'\w*')
-textReDic = {}
-def wordCount(text): #use this function
+
+def wordCount(text):
+    textReDic = {}
     textRe = wordRe.findall(text)
     for word in textRe:
-        if word in textReDic.keys():
-            textReDic[word] += 1
+        if word.lower() in textReDic.keys():
+            textReDic[word.lower()] += 1
         if word != '':
-            textReDic.setdefault(word, 1)
-    pprint.pprint(textReDic)
+            textReDic.setdefault(word.lower(), 1)
+    for k, v in textReDic.items():
+        print(k +': '+ str(v))
